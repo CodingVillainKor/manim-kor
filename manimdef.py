def17ab2c5d3b15d05fcda7d23e4fd25e5d6c0e7
Imcommit <92310362+CodingVillainKor@users.noreply.github.com>
2024-01-16 12:16:35 +0900

    Update manimdef.py

--- manimdef.py
+++ manimdef.py
@@ -1,5 +1,8 @@
 from manim import *
 
+def _find_multiple(string, target):
+    return [i for i in range(len(string)) if string.find(target, i) == i]
+
 class PythonCode(Code):
     def __init__(self, filename, **kwargs):
         kwargs["tab_width"] = kwargs.pop("tab_width", 4)
@@ -16,6 +19,33 @@ class PythonCode(Code):
     @property
     def script(self):
         return self[2]
+    
+    def find_text(self, line_no:int, text:str, nth:int=1):
+        lines = self.code_string.split("\n")
+        line = lines[line_no-1]
+        try:
+            idx = _find_multiple(line, text)[nth-1]
+        except IndexError:
+            raise IndexError(f"Cannot find {nth}th {text} at line {line_no}: {line}")
+
+        indentation_level = line.count(self.indentation_chars)
+        idx -= (len(self.indentation_chars)-1) * indentation_level
+        return idx, idx+len(text)
+    
+    def generate_highlight(self):
+        if getattr(self, "hightlight", None) is not None:
+            raise ValueError("Line highlight already exists")
+        self.highlight = SurroundingRectangle(self.code[0], color=YELLOW_D, stroke_width=DEFAULT_STROKE_WIDTH*0.5)
+
+    def do_highlight(self, line_no:int, remainder_blur=True):
+        if getattr(self, "highlight", None) is None:
+            raise ValueError("There is not highlight. code.generate_highlight() first.")
+        self.highlight.generate_target().surround(self.code[line_no-1]) # TODO: it works not well...
+
+
+
+
+
 
 class NumText(Text):
     def __init__(self, text, **kwargs):
@@ -62,3 +92,14 @@ class DefaultManimClass(MovingCameraScene):
             m.clear_updaters()
         self.playw(*[FadeOut(mob) for mob in self.mobjects])
 
+_surround_buf = DEFAULT_MOBJECT_TO_MOBJECT_BUFFER
+
+class SurroundingRect(Rectangle):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def surround(self, mobject, buf_height=_surround_buf, buf_width=_surround_buf):
+        self.move_to(mobject)\
+            .stretch_to_fit_height(mobject.height + buf_height)\
+            .stretch_to_fit_width(mobject.width + buf_width)
+        return self
