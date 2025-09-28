dc11d48408d7dbeee9fa2340ebff5f7c108921d8
hrim <commit.im@gmail.com>
2025-09-28 11:15:22 +0900

    whykwargs, singlemodel, multimodel

--- src/kwargs2025/main.py
+++ src/kwargs2025/main.py
@@ -75,13 +75,160 @@ class intro(Scene2D):
                 replace_mobject_with_target_in_scene=True,
             ),
             Transform(
-                model_config[0][21], model_[0][-4], replace_mobject_with_target_in_scene=True
+                model_config[0][21],
+                model_[0][-4],
+                replace_mobject_with_target_in_scene=True,
             ),
             Transform(
-                model_config[0][23:25], model_[0][-3:-1], replace_mobject_with_target_in_scene=True
+                model_config[0][23:25],
+                model_[0][-3:-1],
+                replace_mobject_with_target_in_scene=True,
             ),
             FadeOut(model[0][-13:-1]),
             Transform(
                 model[0][-1], model_[0][-1], replace_mobject_with_target_in_scene=True
             ),
         )
+
+
+class whykwargs(Scene2D):
+    def construct(self):
+        c = PythonCode("src/train.py").scale(0.8)
+        c.code[:-2].set_opacity(0.3)
+        c.code[-1].set_opacity(0.3)
+
+        self.playw(FadeIn(c))
+
+        self.playw_return(
+            c.text_slice(6, "**model_config").animate.shift(UP * 0.3), run_time=0.75
+        )
+
+        self.playw(c.code[3].animate.set_opacity(1))
+
+        yml = (
+            Code(code_file="config.yaml", language="yaml", add_line_numbers=False)
+            .scale(0.8)
+            .next_to(c, LEFT)
+        )
+        self.playw(ApplyWave(c.text_slice(4, "model_config"), amplitude=0.1))
+        self.playw(c.code[1:3].animate.set_opacity(1))
+        VGroup(yml, c.generate_target()).arrange(RIGHT, buff=1)
+        self.playwl(MoveToTarget(c), FadeIn(yml), lag_ratio=0.4)
+
+        file_name = (
+            Text("config.yaml", font_size=18)
+            .next_to(yml, UP, buff=0.05)
+            .align_to(yml, LEFT)
+        )
+        self.playw(FadeIn(file_name))
+
+        carr = Arrow(
+            yml.get_right(),
+            c.text_slice(6, "Model").get_top() + LEFT * 0.15,
+            tip_length=0.175,
+            stroke_width=3,
+            buff=0.1,
+        )
+        self.playw(GrowArrow(carr))
+
+        self.play((ymlc := yml.copy()).animate.move_to(c.code[1:3]).scale(0.5))
+        ymlc.set_z_index(1)
+        self.playw(Indicate(c.code[1:3], scale_factor=1.1), FadeOut(ymlc))
+        hl, hlo = c.highlight(4)
+        self.playw(hl, run_time=0.7)
+        self.play(
+            hlo,
+            (cmc := c.text_slice(4, "model_config").copy()).animate.move_to(
+                c.text_slice(6, "model_config")
+            ),
+        )
+        arg_text = (
+            Text("dim=64, depth=6", font_size=24, font=MONO_FONT)
+            .scale(0.8)
+            .move_to(c.text_slice(6, "**model_config"))
+            .align_to(c.text_slice(6, "**model_config"), LEFT)
+        )
+        self.play(FadeOut(cmc))
+        cmc = c.text_slice(6, "**model_config")
+        cmc.save_state()
+        c5p = c.code[5][-1]
+        c5p.save_state()
+        self.playwl(
+            c5p.animate.next_to(arg_text, RIGHT, buff=0.05),
+            Transform(
+                c.text_slice(6, "**model_config"),
+                arg_text,
+            ),
+            lag_ratio=0.3,
+        )
+        self.play(Restore(cmc), Restore(c5p))
+        self.playwl(
+            FadeOut(yml, carr, file_name), c.animate.move_to(ORIGIN), lag_ratio=0.5
+        )
+
+
+class singlemodel(Scene2D):
+    def construct(self):
+        c1 = PythonCode("src/train.py").scale(0.8)
+        c2 = PythonCode("src/train2.py").scale(0.8).align_to(c1, UP)
+        VGroup(c1.code[0], c1.code[-1], c2.code[0], c2.code[-1]).set_opacity(0.3)
+
+        self.addw(c1)
+
+        self.playw(
+            Transform(c1.frame, c2.frame),
+            Transform(c1.code[:4], c2.code[:4]),
+            Transform(
+                c1.text_slice(6, "model = Model("), c2.text_slice(6, "model = Model(")
+            ),
+            Transform(c1.text_slice(6, "**model"), c2.code[6]),
+            Transform(c1.text_slice(6, "_config"), c2.code[7]),
+            Transform(c1.text_slice(6, ")"), c2.text_slice(9, ")")),
+            Transform(c1.code[-1], c2.code[-1]),
+        )
+
+
+class multimodel(Scene2D):
+    def construct(self):
+        c = PythonCode("src/multi.py").scale(0.8).shift(UP * 0.3)
+        VGroup(c.code[0], c.code[-1]).set_opacity(0.3)
+        c.code[4:9].set_opacity(0)
+
+        self.addw(c)
+
+        self.playw(
+            c.code[4:9].animate.set_opacity(1),
+            c.code[1:3].animate.set_opacity(0.3),
+            c.code[-2].animate.set_opacity(0.3),
+        )
+        self.playw(c.animate.shift(LEFT * 2.5))
+
+        m1 = PythonCode("src/Model1.py").scale(0.6)
+        m2 = PythonCode("src/Model2.py").scale(0.6)
+        ms = VGroup(m1, m2).arrange(DOWN, buff=1).next_to(c, RIGHT, buff=0.3)
+
+        self.play(FadeIn(m1))
+        self.playwl(
+            Indicate(m1.text_slice(2, "n_layers")),
+            Indicate(m1.text_slice(2, "hidden_dim")),
+            lag_ratio=0.5,
+        )
+        self.play(FadeIn(m2))
+        self.playwl(
+            Indicate(m2.text_slice(2, "n_embed")),
+            Indicate(m2.text_slice(2, "hidden_dim")),
+            lag_ratio=0.5,
+        )
+        self.playw(
+            Circumscribe(c.code[8]),
+            c.text_slice(5, "models").animate.set_color(YELLOW),
+            c.text_slice(9, "models").animate.set_color(YELLOW),
+        )
+        self.playw(Flash(c.text_slice(6, "Model1", nth=2)))
+        self.playw(Flash(c.text_slice(7, "Model2", nth=2)))
+
+        self.wait(3)
+
+        self.playw(c.code[10].animate.set_opacity(1))
+        self.playw(c.code[1:3].animate.set_opacity(1), c.code[3:].animate.set_opacity(0.3))
+
